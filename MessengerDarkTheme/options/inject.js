ef869e208f12ac80baff729c946c1bd5a0e3e47d
Adam Krška <adam@krska.com>
2019-07-28 19:31:04 +0200

    working toggle for text color

--- MessengerDarkTheme/options/inject.js
+++ MessengerDarkTheme/options/inject.js
@@ -11,6 +11,10 @@ function onGot(item) {
   if (item.colText) {
     colText = item.colText;
   }
+  var colTextDark = "#222222";
+  if (item.colTextDark) {
+    colTextDark = item.colTextDark;
+  }
   var colDark = "#1a1a1a";
   if (item.colDark) {
     colDark = item.colDark;
@@ -21,9 +25,29 @@ function onGot(item) {
   }
   document.documentElement.style.setProperty('--color-background', colBackground);
   document.documentElement.style.setProperty('--color-text', colText);
+  document.documentElement.style.setProperty('--color-textdark', colTextDark);
   document.documentElement.style.setProperty('--color-darkspot', colDark);
   document.documentElement.style.setProperty('--color-highlight', colLight);
-  console.log(item);
+
+  var oppTextCol = [];
+  if (item.oppTextCol) {
+    oppTextCol = item.oppTextCol;
+  }
+
+  var style = '';
+  for (let i = 0; i < oppTextCol.length; i++) {
+    let attr = oppTextCol[i];
+    style +=`
+      ._3058._ui9._hh7[style*="`+attr+`"] > ._aok > ._3oh-,
+      ._3058._ui9._hh7[style*="`+attr+`"] > ._aok > ._3oh- > a {
+        color: var(--color-textdark) !important;
+      }`
+    }
+  var styleSheet = document.createElement("style");
+  styleSheet.id = "styleSheet";
+  styleSheet.innerHTML = style;
+  var ref = document.querySelector('script');
+  ref.parentNode.insertBefore(styleSheet, ref);
 }
 
 var getting = browser.storage.sync.get();
--- MessengerDarkTheme/options/options.js
+++ MessengerDarkTheme/options/options.js
@@ -1,19 +1,41 @@
+var chboxes = document.querySelectorAll("#checkboxes > label > input");
+
 function saveOptions(e) {
+  let checked = [];
+  for (let i = 0; i < chboxes.length; i++) {
+    if (chboxes[i].checked) {
+      checked.push(chboxes[i].value);
+    }
+  }
+
   e.preventDefault();
   browser.storage.sync.set({
     colBackground: document.querySelector("#colBackground").value,
     colText: document.querySelector("#colText").value,
+    colTextDark: document.querySelector("#colTextDark").value,
     colDark: document.querySelector("#colDark").value,
-    colLight: document.querySelector("#colLight").value
+    colLight: document.querySelector("#colLight").value,
+    oppTextCol: checked
   });
 }
 
 function restoreOptions() {
   function setCurrentChoice(result) {
-    document.querySelector("#colBackground").value  = result.colBackground  || "#222222";
-    document.querySelector("#colText").value        = result.colText        || "#efefef";
-    document.querySelector("#colDark").value        = result.colDark        || "#1a1a1a";
-    document.querySelector("#colLight").value       = result.colLight       || "#404040";
+    document.querySelector("#colBackground").value = result.colBackground || "#222222";
+    document.querySelector("#colText").value = result.colText || "#efefef";
+    document.querySelector("#colTextDark").value = result.colTextDark || "#222222";
+    document.querySelector("#colDark").value = result.colDark || "#1a1a1a";
+    document.querySelector("#colLight").value = result.colLight || "#404040";
+    checked = result.oppTextCol || [];
+    for (let i = 0; i < chboxes.length; i++) {
+      chboxes[i].checked = false;
+      for (let j = 0; j < checked.length; j++) {
+        if (chboxes[i].value == checked[j]) {
+          chboxes[i].checked = true;
+        }
+      }
+    }
+    updateElementCol();
   }
 
   function onError(error) {
@@ -25,13 +47,25 @@ function restoreOptions() {
 }
 
 function resetOptions() {
-  document.querySelector("#colBackground").value  = "#222222";
-  document.querySelector("#colText").value        = "#efefef";
-  document.querySelector("#colDark").value        = "#1a1a1a";
-  document.querySelector("#colLight").value       = "#404040";
+  document.querySelector("#colBackground").value = "#222222";
+  document.querySelector("#colText").value = "#efefef";
+  document.querySelector("#colTextDark").value = "#222222";
+  document.querySelector("#colDark").value = "#1a1a1a";
+  document.querySelector("#colLight").value = "#404040";
+  for (let i = 0; i < chboxes.length; i++) {
+    chboxes[i].checked = false;
+  }
+  updateElementCol();
+}
+
+function updateElementCol() {
+  document.documentElement.style.setProperty('--color-background', document.querySelector("#colBackground").value);
+  document.documentElement.style.setProperty('--color-text', document.querySelector("#colText").value);
+  document.documentElement.style.setProperty('--color-textdark', document.querySelector("#colTextDark").value);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.querySelectorAll("input[type=color]").forEach(input => input.addEventListener("change", updateElementCol));
+document.getElementById("savebtn").addEventListener("click", saveOptions);
 document.getElementById("resetbtn").addEventListener("click", resetOptions);
 document.getElementById("revertbtn").addEventListener("click", restoreOptions);
